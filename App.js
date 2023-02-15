c2c89070f7f23f17a5e5e9b2919592c8d05b3ce5
Your Name <you@example.com>
2023-02-15 16:36:48 -0300

    Aula 15-02

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HomeScreen />
     </View>
   );
 }
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>Sou a HomeScreen</Text>
+      <Image
+        source={
+          {
+            uri: 'https://picsum.photos/300/300'
+          }
+        }
+        style={
+          {
+            height: '300px',
+            width: '300px'
+          }
+        }
+      />
+    </View>
+  );
+}
+// SobreScreen
+// Seu nome e um oi e uma frase de impacto!
+// ContatoScreen
+// Um página de contato informando como falar com você
+// SurpresaScreen
+// O que você desejar inserir nesta página e SE CONSEGUIR uma imagem.
+// Ref imagem: https://reactnative.dev/docs/image
 
 const styles = StyleSheet.create({
   container: {
